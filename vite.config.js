65dec84fccbc00df5fedde845e5a3a8eb4f674c1
Vinod Chaudhary <vinodchoudhari371@gmail.com>
2025-08-07 18:14:20 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"bmicalculator",
+  base:"/bmicalculator/",
   plugins: [tailwindcss(),
     react()],
 })
